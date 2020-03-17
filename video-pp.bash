5f3c1041a2134e54a681e1f0ca013efa12e9d0a2
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-03-17 07:28:26 -0400

    Update video-pp.bash

--- video-pp.bash
+++ video-pp.bash
@@ -1,5 +1,14 @@
 #!/usr/bin/with-contenv bash
 
+# check for video files
+if find "$1" -type f  -iregex ".*/.*\.\(mkv\|mp4\|avi\)" | read; then
+	echo "CHECK: Finding video files for processing..."
+	echo "SUCCESS: Video files found"
+else
+	echo "ERROR: No video files found for processing"
+	exit 1
+fi
+
 # Manual run of Sickbeard MP4 Automator
 python3 /usr/local/sma/manual.py -i "$1" -nt
 
