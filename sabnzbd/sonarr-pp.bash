a75b299106405fc25f66b58bd47a4eace91440eb
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-03-18 17:17:41 -0400

    Update sonarr-pp.bash

--- sabnzbd/sonarr-pp.bash
+++ sabnzbd/sonarr-pp.bash
@@ -4,6 +4,12 @@ set -e
 
 # start
 
+# Create log
+if [ ! -f "/config/scripts/logs/sonarr-pp.log" ]; then
+	touch "/config/scripts/logs/sonarr-pp.log"
+	chmod 0666 "/config/scripts/logs/sonarr-pp.log"
+fi
+
 bash /usr/local/sabnzbd-scripts/video-pp.bash "$1" "/config/scripts/configs/sonarr-pp.ini" 2>&1 | tee "/config/scripts/logs/sonarr-pp.log"
 
 exit 0 
