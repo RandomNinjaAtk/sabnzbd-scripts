f7eda43da8e11a4d1923fcabe5dccb89e6f830ca
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-03-18 17:18:06 -0400

    Update radarr-pp.bash

--- sabnzbd/radarr-pp.bash
+++ sabnzbd/radarr-pp.bash
@@ -4,6 +4,13 @@ set -e
 
 # start
 
+# Create log
+if [ ! -f "/config/scripts/logs/radarr-pp.log" ]; then
+	touch "/config/scripts/logs/radarr-pp.log"
+	chmod 0666 "/config/scripts/logs/radarr-pp.log"
+fi
+
+
 bash /usr/local/sabnzbd-scripts/video-pp.bash "$1" "/config/scripts/configs/radarr-pp.ini" 2>&1 | tee "/config/scripts/logs/radarr-pp.log"
 
 exit 0 
