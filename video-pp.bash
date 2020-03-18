442dc2751c6df0c875fdb1b1d43bf084992706eb
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-03-18 08:59:38 -0400

    Update video-pp.bash

--- video-pp.bash
+++ video-pp.bash
@@ -4,11 +4,60 @@ set -e
 
 # start
 
-echo "Video Language setting: ${VIDEO_LANG}"
+find "$1" -type f -iregex ".*/.*\.\(mkv\|mp4\|avi\)" -print0 | while IFS= read -r -d '' video; do
+	tracks=$(mkvmerge -J "$video")
+	if [ -z "${tracks}" ]; then
+		allvideo=$(echo "${tracks}" | jq ".tracks[] | select(.type==\"video\") | .id")
+		allaudio=$(echo "${tracks}" | jq ".tracks[] | select(.type==\"audio\") | .id")
+		allsub=$(echo "${tracks}" | jq ".tracks[] | select(.type==\"subtitles\") | .id")		
+		setaudio=$(echo "${tracks}" | jq ".tracks[] | select((.type==\"audio\") and select(.properties.language==\"${VIDEO_LANG}\")) | .id")
+		undaudio=$(echo "${tracks}" | jq '.tracks[] | select((.type=="audio") and select(.properties.language=="und")) | .id')
+		nonundaudio=$(echo "${tracks}" | jq '.tracks[] | select((.type=="audio") and select(.properties.language!="und")) | .id')
+		nonperfaudio=$(echo "${tracks}" | jq ".tracks[] | select((.type==\"audio\") and select(.properties.language!=\"${VIDEO_LANG}\")) | .id")
+		perfsub=$(echo "${tracks}" | jq ".tracks[] | select((.type==\"subtitles\") and select(.properties.language==\"${VIDEO_LANG}\")) | .id")
+		nonperfsub=$(echo "${tracks}" | jq ".tracks[] | select((.type==\"subtitles\") and select(.properties.language!=\"${VIDEO_LANG}\")) | .id")
+	else
+		echo "ERROR: mkvmerge failed to read tracks and set values"
+		rm "$video" && echo "INFO: deleted: $video"
+	fi
+	
+	if [ -z "${allvideo}" ]; then
+		echo "video tracks found"
+	fi
+	
+	if [ -z "${allaudio}" ]; then
+		echo "audio tracks found"
+	fi
+	
+	if [ -z "${allsub}" ]; then
+		echo "subtitles tracks found"
+	fi
+	
+	if [ -z "${setaudio}" ]; then
+		echo "${VIDEO_LANG} audio tracks found, id: ${setaudio}"
+	fi
+	
+	if [ -z "${undaudio}" ]; then
+		echo "und audio tracks found, id: ${undaudio}"
+	fi
+	
+	if [ -z "${nonperfaudio}" ]; then
+		echo "non ${VIDEO_LANG} audio tracks found, id: ${nonperfaudio}"
+	fi
+	
+	if [ -z "${perfsub}" ]; then
+		echo "${VIDEO_LANG} audio tracks found, id: ${perfsub}"
+	fi
+	
+	if [ -z "${nonperfsub}" ]; then
+		echo "${VIDEO_LANG} audio tracks found, id: ${nonperfsub}"
+	fi
+	
+done
 sleep 5
 
 # check for video files
-if find "$1" -type f  -iregex ".*/.*\.\(mkv\|mp4\|avi\)" | read; then
+if find "$1" -type f -iregex ".*/.*\.\(mkv\|mp4\|avi\)" | read; then
 	echo "CHECK: Finding video files for processing..."
 	echo "SUCCESS: Video files found"
 else
@@ -20,7 +69,7 @@ fi
 python3 /usr/local/sma/manual.py --config "$2" -i "$1" -nt
 
 # check for video files
-if find "$1" -type f  -iregex ".*/.*\.\(mkv\|mp4\)" | read; then
+if find "$1" -type f -iregex ".*/.*\.\(mkv\|mp4\)" | read; then
 	echo "video-pp processing complete!"
 else
 	echo "ERROR: No video files"
