96b57064aec1f14d690b767f92ecee7d86f85079
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-03-18 10:49:13 -0400

    Update video-pp.bash

--- video-pp.bash
+++ video-pp.bash
@@ -4,20 +4,28 @@ set -e
 
 # start
 
+# check for video files
+if find "$1" -type f -iregex ".*/.*\.\(mkv\|mp4\|avi\)" | read; then
+	echo "CHECK: Finding video files for processing..."
+	echo "SUCCESS: Video files found"
+else
+	echo "ERROR: No video files found for processing"
+	exit 1
+fi
+
+
 find "$1" -type f -iregex ".*/.*\.\(mkv\|mp4\|avi\)" -print0 | while IFS= read -r -d '' video; do
-	tracks=$(mkvmerge -J "$video")
+	tracks=$(ffprobe -show_streams -print_format json -loglevel quiet "$video")
 	if [ ! -z "${tracks}" ]; then
-		allvideo=$(echo "${tracks}" | jq ".tracks[] | select(.type==\"video\") | .id")
-		allaudio=$(echo "${tracks}" | jq ".tracks[] | select(.type==\"audio\") | .id")
-		allsub=$(echo "${tracks}" | jq ".tracks[] | select(.type==\"subtitles\") | .id")		
-		setaudio=$(echo "${tracks}" | jq ".tracks[] | select((.type==\"audio\") and select(.properties.language==\"${VIDEO_LANG}\")) | .id")
-		undaudio=$(echo "${tracks}" | jq '.tracks[] | select((.type=="audio") and select(.properties.language=="und")) | .id')
-		nonundaudio=$(echo "${tracks}" | jq '.tracks[] | select((.type=="audio") and select(.properties.language!="und")) | .id')
-		nonperfaudio=$(echo "${tracks}" | jq ".tracks[] | select((.type==\"audio\") and select(.properties.language!=\"${VIDEO_LANG}\")) | .id")
-		perfsub=$(echo "${tracks}" | jq ".tracks[] | select((.type==\"subtitles\") and select(.properties.language==\"${VIDEO_LANG}\")) | .id")
-		nonperfsub=$(echo "${tracks}" | jq ".tracks[] | select((.type==\"subtitles\") and select(.properties.language!=\"${VIDEO_LANG}\")) | .id")
+		allvideo=$(echo "${tracks}" | jq '.streams | .[] | select (.codec_type=="video") | .index')
+		allaudio=$(echo "${tracks}" | jq '.streams | .[] | select (.codec_type=="audio") | .index')
+		allsub=$(echo "${tracks}" | jq '.streams | .[] | select (.codec_type=="subtitle") | .index')	
+		setaudio=$(echo "${tracks}" | jq ".streams | .[] | select(.codec_type=="audio") | select(.tags.language==\"${VIDEO_LANG}\") | .index")
+		undaudio=$(echo "${tracks}" | jq ".streams | .[] | select(.codec_type=="audio") | select(.tags.language==\"und\") | .index")
+		nonundaudio=$(echo "${tracks}" | jq ".streams | .[] | select(.codec_type=="audio") | select(.tags.language!=\"und\") | .index")
+		setsub=$(echo "${tracks}" | jq ".streams | .[] | select(.codec_type=="subtitle") | select(.tags.language==\"${VIDEO_LANG}\") | .index")
 	else
-		echo "ERROR: mkvmerge failed to read tracks and set values"
+		echo "ERROR: ffprobe failed to read tracks and set values"
 		rm "$video" && echo "INFO: deleted: $video"
 	fi
 	
@@ -43,23 +51,14 @@ find "$1" -type f -iregex ".*/.*\.\(mkv\|mp4\|avi\)" -print0 | while IFS= read -
 		echo "${VIDEO_LANG} audio tracks found, id: ${setaudio}"
 	elif [ ! -z "${undaudio}" ]; then
 		echo "und audio tracks found, id: ${undaudio}"
-	elif [ -z "${perfsub}" ]; then
-		echo "${VIDEO_LANG} audio tracks found, id: ${perfsub}"
+	elif [ -z "${setsub}" ]; then
+		echo "${VIDEO_LANG} audio tracks found, id: ${setsub}"
 	else
 		echo "ERROR: no ${VIDEO_LANG} audio/subtitle tracks found"
 		rm "$video" && echo "INFO: deleted: $video"
 	fi	
 done
 sleep 5
-
-# check for video files
-if find "$1" -type f -iregex ".*/.*\.\(mkv\|mp4\|avi\)" | read; then
-	echo "CHECK: Finding video files for processing..."
-	echo "SUCCESS: Video files found"
-else
-	echo "ERROR: No video files found for processing"
-	exit 1
-fi
 
 # Manual run of Sickbeard MP4 Automator
 python3 /usr/local/sma/manual.py --config "$2" -i "$1" -nt
