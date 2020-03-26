b9a2e78b4fa0105505098bb4ccd4a1fc07db2739
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-03-25 20:18:43 -0400

    Update video-pp.bash

--- sabnzbd/video-pp.bash
+++ sabnzbd/video-pp.bash
@@ -1,24 +1,8 @@
-#!/usr/bin/bash
+#!/usr/bin/with-contenv bash
 
 # start
 
 set -e
-
-if [ ! -f /config/scripts/Deobfuscate.py ]; then
-    echo "downloading Deobfuscate.py from: https://github.com/sabnzbd/sabnzbd/blob/develop/scripts/Deobfuscate.py"
-    curl -o /config/scripts/Deobfuscate.py https://raw.githubusercontent.com/sabnzbd/sabnzbd/develop/scripts/Deobfuscate.py
-    echo "done"
-
-    # Set Permissions
-    echo "setting permissions..."
-    chmod 777 /config/scripts/Deobfuscate.py
-    echo "done"
-fi
-
-# Execute on new downloads
-cd /config/scripts
-
-timeout --foreground 1m python Deobfuscate.py "$@"
 
 bash /usr/local/sabnzbd-scripts/video-pp.bash "$1" "/usr/local/sma/config/autoProcess.ini"
 
