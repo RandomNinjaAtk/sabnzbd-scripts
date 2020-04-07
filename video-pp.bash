83605452aca39b555cbbf5a708797cc14cd199d5
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-04-06 21:46:06 -0400

    Update video-pp.bash

--- video-pp.bash
+++ video-pp.bash
@@ -112,12 +112,12 @@ find "$1" -type f -iregex ".*/.*\.\(mkv\|mp4\|avi\)" -print0 | while IFS= read -
 				echo "$VideoTrackCount \"unknown\" video language track found, re-tagging as \"${VIDEO_LANG}\""
 				MKVvideo=" -d ${VideoTrack} --language ${VideoTrack}:${VIDEO_LANG}"
 			else
-				echo "$VideoTrackCount \"foreign\" video tracks found!"
+				foreignvideo="true"
 				SetVideoLanguage="false"
 				MKVvideo=""
 			fi
 		else
-			echo "$VideoTrackCount \"${VIDEO_LANG}\" video tracks found!"
+			echo "$VideoTrackCount video track found!"
 			SetVideoLanguage="false"
 			MKVvideo=""
 		fi
