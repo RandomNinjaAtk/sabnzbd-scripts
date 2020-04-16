8982babfd173047297936ca5e5bd80ab6e0dfd21
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-04-16 10:09:04 -0400

    Better error messaging

--- video-pp.bash
+++ video-pp.bash
@@ -42,6 +42,7 @@ find "$1" -type f -iregex ".*/.*\.\(mkv\|mp4\|avi\)" -print0 | while IFS= read -
 		# audio preferred language
 		AudioTracksLanguage=$(echo "${tracks}" | jq ".tracks[] | select((.type==\"audio\") and select(.properties.language==\"${VIDEO_LANG}\")) | .id")
 		AudioTracksLanguageCount=$(echo "${tracks}" | jq ".tracks[] | select((.type==\"audio\") and select(.properties.language==\"${VIDEO_LANG}\")) | .id" | wc -l)
+		AudioTracksLanguageFound=$(echo "${tracks}" | jq ".tracks[] | select((.type==\"audio\") | .properties.language")
 		# audio unkown laguage
 		AudioTracksLanguageUND=$(echo "${tracks}" | jq ".tracks[] | select((.type==\"audio\") and select(.properties.language==\"und\")) | .id")
 		AudioTracksLanguageUNDCount=$(echo "${tracks}" | jq ".tracks[] | select((.type==\"audio\") and select(.properties.language==\"und\")) | .id" | wc -l)
@@ -55,6 +56,8 @@ find "$1" -type f -iregex ".*/.*\.\(mkv\|mp4\|avi\)" -print0 | while IFS= read -
 		# subtitle preferred langauge
 		SubtitleTracksLanguage=$(echo "${tracks}" | jq ".tracks[] | select((.type==\"subtitles\") and select(.properties.language==\"${VIDEO_LANG}\")) | .id")
 		SubtitleTracksLanguageCount=$(echo "${tracks}" | jq ".tracks[] | select((.type==\"subtitles\") and select(.properties.language==\"${VIDEO_LANG}\")) | .id" | wc -l)
+		SubtitleTracksLanguageFound=$(echo "${tracks}" | jq ".tracks[] | select((.type==\"subtitles\") | .properties.language")
+
 	else
 		echo "ERROR: ffprobe failed to read tracks and set values"
 		rm "$video" && echo "INFO: deleted: $video"
@@ -95,6 +98,12 @@ find "$1" -type f -iregex ".*/.*\.\(mkv\|mp4\|avi\)" -print0 | while IFS= read -
 				continue
 			fi
 		else
+			if [ ! -z "${AudioTracks}" ]; then
+				echo "INFO: \"${AudioTracksLanguageFound}\" audio track found!"
+			fi
+			if [ ! -z "${SubtitleTracks}" ]; then
+				echo "INFO: \"${SubtitleTracksLanguageFound}\" subtitle track found!"
+			fi
 			echo "ERROR: No \"${VIDEO_LANG}\" audio or subtitle tracks found..."
 			# rm "$video" && echo "INFO: deleted: $filename"
 			exit 1
@@ -152,6 +161,12 @@ find "$1" -type f -iregex ".*/.*\.\(mkv\|mp4\|avi\)" -print0 | while IFS= read -
 				MKVaudio=""
 			fi
 		elif [ -z "$SubtitleTracksLanguage" ]; then
+			if [ ! -z "${AudioTracks}" ]; then
+				echo "INFO: \"${AudioTracksLanguageFound}\" audio track found!"
+			fi
+			if [ ! -z "${SubtitleTracks}" ]; then
+				echo "INFO: \"${SubtitleTracksLanguageFound}\" subtitle track found!"
+			fi
 			echo "ERROR: no \"${VIDEO_LANG}\" audio/subtitle tracks found!"
 			# rm "$video" && echo "INFO: deleted: $filename"
 			exit 1
