eba4d9c8097c67ff96ab292d8285afd4d4df2f26
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-05-26 15:07:01 -0400

    Migrate to separate radarr/sonarr config files

--- sabnzbd/video-pp.bash
+++ sabnzbd/video-pp.bash
@@ -17,6 +17,12 @@ fi
 
 timeout --foreground 1m python3 /config/scripts/Deobfuscate.py "$@"
 
-bash /usr/local/sabnzbd-scripts/video-pp.bash "$1" "/config/scripts/configs/video-pp-sma.ini"
+if [ -f "/config/scripts/configs/video-pp-sma.ini" ]; then
+    cp "/config/scripts/configs/video-pp-sma.ini" "/config/scripts/configs/radarr-sma.ini" && \
+    cp "/config/scripts/configs/video-pp-sma.ini" "/config/scripts/configs/sonarr-sma.ini" && \
+    rm "/config/scripts/configs/video-pp-sma.ini"
+fi
+
+bash /usr/local/sabnzbd-scripts/video-pp.bash "$1" "/config/scripts/configs/$5-sma.ini"
 
 exit $?
